34644e41bc1e82be4f64e0306e6d03b25d91e105
Kutullo innocent Moropane <49004939+IKcracker@users.noreply.github.com>
2024-03-25 00:22:47 +0200

    new

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
+let sidenav = document.querySelector('.sideIcon');
+let sideMenu = document.querySelector('.sideBar');
+let closeSide = document.querySelector('.close');
 
+
+sidenav.addEventListener("click", () => {
+
+    sideMenu.style.display = 'block';
+
+});
+
+closeSide.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+})
