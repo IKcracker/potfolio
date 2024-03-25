0a7793fc8e694b344212d9e10a323fa5493aead1
Kutullo innocent Moropane <49004939+IKcracker@users.noreply.github.com>
2024-03-25 16:23:08 +0200

    read more btn

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+
+
+//navbar 
+
 let sidenav = document.querySelector('.sideIcon');
 let sideMenu = document.querySelector('.sideBar');
 let closeSide = document.querySelector('.close');
@@ -19,3 +23,44 @@ for (let link of links) {
         sideMenu.style.display = 'none';
     })
 }
+
+//functionality
+
+let about = document.querySelector('.aboutBtn');
+let aboutP = document.querySelector('.about-content p');
+console.dir(about);
+about.addEventListener('click', () => {
+
+
+    if (about.classList.toggle('aboutBtn')) {
+        aboutP.innerText = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore, in quaerat! Dicta recusandae
+    doloremque
+    dolores deleniti est, dignissimos, itaque doloribus tempora hic libero laboriosam nesciunt culpa
+    corrupti sint dolore sequi.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore, in quaerat! Dicta recusandae
+    doloremque`;
+
+        about.innerText = 'read more';
+        console.log(about.classList.toggle('aboutBtn'));
+
+    }
+    else {
+        aboutP.innerText = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore, in quaerat! Dicta recusandae
+    doloremque
+    dolores deleniti est, dignissimos, itaque doloribus tempora hic libero laboriosam nesciunt culpa
+    corrupti sint dolore sequi.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore, in quaerat! Dicta recusandae
+    doloremque
+    dolores deleniti est, dignissimos, itaque doloribus tempora hic libero laboriosam nesciunt culpa
+    corrupti sint dolore sequi.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore, in quaerat! Dicta recusandae
+    doloremque
+    dolores deleniti est, dignissimos, itaque doloribus tempora hic libero laboriosam nesciunt culpa
+    corrupti sint dolore sequi.`;
+        about.innerText = 'read less'
+        console.log(about.classList.toggle('aboutBtn'));
+
+    }
+
+
+    about.classList.toggle('aboutBtn');
+
+})
+
